ad3659d4565eae102541748077d576f8b5fa8655
ptanlam <phanlam20033002@gmail.com>
2021-10-11 21:14:26 +0700

    implemented upload file and save file location

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,16 +1,17 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
+import { map } from 'rxjs';
 import { FilesService } from './files.service';
 import { FilesByOwnerId } from './interfaces/FilesByOwnerId.interface';
-import { Observable } from 'rxjs';
-import { File } from './entities/file.entity';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly _filesService: FilesService) {}
 
   @GrpcMethod('FilesService', 'GetAllForOwner')
-  getAllForOwner(data: FilesByOwnerId): Observable<Array<File>> {
-    return this._filesService.getAllForOwner(data.ownerId);
+  getAllForOwner(data: FilesByOwnerId) {
+    return this._filesService
+      .getAllForOwner(data.ownerId)
+      .pipe(map((files) => ({ files })));
   }
 }
